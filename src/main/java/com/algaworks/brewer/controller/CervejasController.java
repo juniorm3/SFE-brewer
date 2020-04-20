8fe06cf483345b0436db30a2dd14ad31dee9d233
Mario <mario.martins@sonda.com>
2020-04-20 15:10:53 -0300

    Baixando e configurando o layout no projeto

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -30,4 +30,9 @@ public class CervejasController {
 		System.out.println(">>> sku: " + cerveja.getSku());
 		return "redirect:/cervejas/novo";
 	}
+	
+	@RequestMapping("/cervejas/cadastro")
+	public String cadastro() {
+		return "cerveja/cadastro-produto";
+	}
 }
