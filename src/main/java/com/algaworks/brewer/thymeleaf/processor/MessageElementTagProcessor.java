dadbf392c9fc35d64427537fd7e4ca20321c6a7a
Mario <juniorm3@msn.com>
2020-05-19 21:04:28 -0300

    Desafio: Removendo duplicação da tag html

--- src/main/java/com/algaworks/brewer/thymeleaf/processor/MessageElementTagProcessor.java
+++ src/main/java/com/algaworks/brewer/thymeleaf/processor/MessageElementTagProcessor.java
@@ -23,8 +23,8 @@ public class MessageElementTagProcessor extends AbstractElementTagProcessor {
 		IModelFactory modelFactory = context.getModelFactory();
 		
 		IModel model = modelFactory.createModel();
-		model.add(modelFactory.createStandaloneElementTag("th:block", "th:include", "fragments/MensagensSucesso"));
-		model.add(modelFactory.createStandaloneElementTag("th:block", "th:include", "fragments/MensagensErroValidacao"));
+		model.add(modelFactory.createStandaloneElementTag("th:block", "th:replace", "fragments/MensagensSucesso :: alert"));
+		model.add(modelFactory.createStandaloneElementTag("th:block", "th:replace", "fragments/MensagensErroValidacao :: alert"));
 		
 		structureHandler.replaceWith(model, true);
 	}
