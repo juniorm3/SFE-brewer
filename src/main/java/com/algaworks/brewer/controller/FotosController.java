c045662fb3448b24bdc9c6f645306ff61a492251
Mario <mario.martins@sonda.com>
2020-06-01 11:14:52 -0300

    Salvando o nome da foto da cerveja

--- src/main/java/com/algaworks/brewer/controller/FotosController.java
+++ src/main/java/com/algaworks/brewer/controller/FotosController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.algaworks.brewer.dto.FotoDTO;
 import com.algaworks.brewer.storage.FotoStorageRunnable;
 
 @RestController
@@ -14,13 +15,11 @@ import com.algaworks.brewer.storage.FotoStorageRunnable;
 public class FotosController {
 
 	@PostMapping
-	public DeferredResult<String> upload(@RequestParam("files[]") MultipartFile[] files) {
-		DeferredResult<String> resultado = new DeferredResult<>();
+	public DeferredResult<FotoDTO> upload(@RequestParam("files[]") MultipartFile[] files) {
+		DeferredResult<FotoDTO> resultado = new DeferredResult<>();
 		
 		Thread thread = new Thread(new FotoStorageRunnable(files, resultado));
 		thread.start();
-		
-		
 		
 		return resultado;
 	}
